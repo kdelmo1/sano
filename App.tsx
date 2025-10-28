f260ea222b8f9dbac480cdfee3d60c517d818546
Alonso Isauro <aisauro@ucsc.edu>
2025-10-27 19:56:20 -0700

    initial google button

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/*
 import { StrictMode } from "react";
 import { useState } from 'react';
 import { View } from 'react-native';
@@ -27,3 +28,53 @@ export default function App() {
     </StrictMode>
 	)
 }
+*/
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabase'
+import { View, Text } from 'react-native'
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Session } from '@supabase/supabase-js'
+
+import LoginScreen from './screens/auth/loginScreen';
+import Home from './screens/home/home';
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleLoginSuccess = () => {
+		setIsLoggedIn(true)
+	}
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    /*<View>
+      {session ? (
+        <Text>Welcome, {session.user.email}</Text>
+      ) : (
+        <LoginScreen />
+        //<LoginScreen onLoginSuccess={handleLoginSuccess} />
+      )}
+    </View>
+    */
+    <SafeAreaProvider>
+      <View>
+        {isLoggedIn ? (
+          <Home />
+        ) : (
+          <LoginScreen onLoginSuccess={handleLoginSuccess} />
+        )}
+      </View>
+    </SafeAreaProvider>
+  )
+}
+//
