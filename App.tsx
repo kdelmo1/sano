9f3d35b9540e54735ef2a7ba27637b919bdcf912
Alonso Isauro <aisauro@ucsc.edu>
2025-11-18 06:58:07 -0800

    new suff?

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-/*
-import { StrictMode } from "react";
-import { useState } from 'react';
-import { View } from 'react-native';
-=======
 import { StrictMode, useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
->>>>>>> chat
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./src/screens/auth/loginScreen";
 import Home from "./src/screens/home/Home";
@@ -62,58 +55,6 @@ export default function App() {
     </AuthContext.Provider>
   );
 }
-<<<<<<< HEAD
-*/
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import { View, Text } from 'react-native'
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Session } from '@supabase/supabase-js'
-
-import LoginScreen from './screens/auth/loginScreen';
-import Home from './screens/home/home';
-
-export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  const handleLoginSuccess = () => {
-		setIsLoggedIn(true)
-	}
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-
-  return (
-    /*<View>
-      {session ? (
-        <Text>Welcome, {session.user.email}</Text>
-      ) : (
-        <LoginScreen />
-        //<LoginScreen onLoginSuccess={handleLoginSuccess} />
-      )}
-    </View>
-    */
-    <SafeAreaProvider>
-      <View>
-        {isLoggedIn ? (
-          <Home />
-        ) : (
-          <LoginScreen onLoginSuccess={handleLoginSuccess} />
-        )}
-      </View>
-    </SafeAreaProvider>
-  )
-}
-//
-=======
 
 const styles = StyleSheet.create({
   container: {
@@ -122,4 +63,3 @@ const styles = StyleSheet.create({
     height: "100%",
   },
 });
->>>>>>> chat
